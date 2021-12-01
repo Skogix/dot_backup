e202df2e83cad7d31ec94013f5f63cbea382ea5d
Emil Skogsund <skogix@gmail.com>
2021-12-01 15:26:21 +0100

    Modified 006-dirs.sh, added dotfiles/plain/common/.config/i3/i3blocks-original.conf, added dotfiles/plain/common/.config/variety/variety.conf, modified filelist

--- 006-dirs.sh
+++ 006-dirs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ~
 rm -rf Desktop Documents Downloads Music Pictures Public Templates Videos
-mkdir data .desktop data/docs tmp .music data/pics .public .templates data/vids
+mkdir data .desktop data/docs data/pics/wallpapers data/pics/favoriteWallpapers tmp .music data/pics .public .templates data/vids
