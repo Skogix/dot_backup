4e5227add8bbb7a530dff68e41a0b17e08010562
Emil Skogsund <skogix@gmail.com>
2021-12-04 19:09:21 +0100

    UPDATEALL:lör  4 dec 2021 19:09:16 CET:

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 currentTime=$(date)
-~/dot/lazygit.sh "UPDATEALL:$currentTime: $1"
 cd ~/data
+~/dot/lazygit.sh "UPDATEALL:$currentTime: $1"
+cd ~/dot
+~/dot/lazygit.sh "UPDATEALL:$currentTime: $1"
+cd ~/dev
+~/dot/lazygit.sh "UPDATEALL:$currentTime: $1"
+cd ~/org
+~/dot/lazygit.sh "UPDATEALL:$currentTime: $1"
+cd ~/skogix.github.io
 ~/dot/lazygit.sh "UPDATEALL:$currentTime: $1"
